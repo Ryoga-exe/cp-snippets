9ae9f76f1afb42e0e532e425a4a9f228996a7f9b
Ryoga-exe <git@ryoga.dev>
2024-01-10 09:13:19 +0900

    feat: add cpp template

---
+++ src/cpp/template/templaate.cpp
@@ -0,0 +1,11 @@
+/// @prefix cpp
+/// @description template : 競プロ用テンプレート
+/// @isFileTemplate true
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+    /** $0 **/
+    return 0;
+}
