a8e0c3efa21aff851877a0298cbae415205cd58c
Ryoga-exe <git@ryoga.dev>
2024-01-10 09:50:51 +0900

    feat: add UnionFind

---
+++ src/cpp/data-structure/UnionFind.hpp
@@ -0,0 +1,34 @@
+#include <cstddef>
+#include <utility>
+#include <vector>
+using namespace std;
+
+/// start
+/// @prefix cpUnionFind
+/// @description UnionFind (Disjoint Set Union) : 素集合データ構造 \n O(𝛼(n))
+/// @isFileTemplate false
+struct UnionFind {
+    vector<int> par;
+    UnionFind(const size_t n) : par(n, -1) {}
+    inline void reset(const size_t n) {
+        par.assign(n, -1);
+    }
+    inline int root(const size_t x) noexcept {
+        return (par[x] < 0 ? x : par[x] = root(par[x]));
+    }
+    inline bool unite(size_t x, size_t y) noexcept {
+        x = root(x);
+        y = root(y);
+        if (x == y) return false;
+        if (par[x] > par[y]) swap(x, y);
+        par[x] += par[y];
+        par[y] = x;
+        return true;
+    }
+    inline bool same(const size_t x, const size_t y) noexcept {
+        return root(x) == root(y);
+    }
+    inline int size(const size_t x) noexcept {
+        return -par[root(x)];
+    };
+};
