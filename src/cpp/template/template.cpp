e000b507ee2f39e80245c20ad6359d77c56620ff
Ryoga-exe <git@ryoga.dev>
2024-01-10 13:25:16 +0900

    fix: rename

--- src/cpp/template/template.cpp
+++ src/cpp/template/template.cpp
@@ -1,6 +1,6 @@
-/// @prefix cpp
-/// @description template : 競プロ用テンプレート
-/// @isFileTemplate true
+/// @prefix: cpp
+/// @description: template : 競プロ用テンプレート
+/// @filetemplate: true
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
